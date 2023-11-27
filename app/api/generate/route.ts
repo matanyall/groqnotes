92dbe265994b1aaf75cd583286bccbca67ba1a49
Matanya Loewenthal <matanya@loewenthal.net>
2023-11-27 14:14:59 -0700

    Update route.ts

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -49,7 +49,8 @@ export async function POST(req: Request): Promise<Response> {
         content:
           "You are an AI writing assistant that continues existing text based on context from prior text. " +
           "Give more weight/priority to the later characters than the beginning ones. " +
-          "Limit your response to no more than 200 characters, but make sure to construct complete sentences.",
+          "Limit your response to no more than 200 characters, but make sure to construct complete sentences. " +
+          "You only ever respond with the completion text.",
         // we're disabling markdown for now until we can figure out a way to stream markdown text with proper formatting: https://github.com/steven-tey/novel/discussions/7
         // "Use Markdown formatting when appropriate.",
       },
